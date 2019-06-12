d0361822dc3f34b659fdc31127ec9c66800977e6
modaresimr <modaresi.mr@gmail.com>
2019-06-12 18:44:48 +0200

    sd

--- pods-tools.php
+++ pods-tools.php
@@ -42,7 +42,7 @@ add_action( 'pods_api_post_save_pod_item', 'my_title_content_pods_update_terms_o
  */ 
 function my_title_content_pods_update_terms_on_save( $pieces, $is_new_item, $id ) { 
 	
-	wp_die(var_export($pieces,true));
+	wp_die(var_export($pieces['fields'],true));
 	
    if ( ! wp_is_post_revision( $id ) ) { 
         // Avoid recursion loops on saving. 
